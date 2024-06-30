a62b1e8e3917c4d8e23122df5795bae202ef13b6
Marco <meroco5212@gmail.com>
2024-06-30 11:47:26 -0500

    comenta-remov

--- src/main/java/com/example/proyecto/web/transporte/guia_remision/GuiaRemisionService.java
+++ src/main/java/com/example/proyecto/web/transporte/guia_remision/GuiaRemisionService.java
@@ -30,25 +30,25 @@ public class GuiaRemisionService implements IGuiaRemisionService {
         return data.findById(id);
     }
 
-    // Método para guardar una nueva guía de remisión o actualizar una existente
+
     @Override
     public void Guardar(GuiaRemision f) {
         data.save(f);
     }
 
-    // Método para eliminar una guía de remisión por su ID
+
     @Override
     public void Eliminar(int id) {
         data.deleteById(id);
     }
 
-    // Método para buscar guías de remisión que coincidan con un criterio específico
+
     @Override
     public List<GuiaRemision> Buscar(String dato) {
         return data.buscarPorTodo(dato);
     }
 
-    // Métodos para listar todas las guías de remisión en orden ascendente y descendente
+
     @Override
     public List<GuiaRemision> ListarOrdenAscendente() {
         return data.OrdenAscendente();
